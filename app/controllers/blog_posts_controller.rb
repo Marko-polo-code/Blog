a5c0c6301bc5fa353aa6644a819be60d325dc9a7
Mark Muller <mark.muller.w@gmail.com>
2023-08-29 17:44:40 +0200

    added a delete button

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,10 +1,12 @@
 class BlogPostsController < ApplicationController
+before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
+    @set_blog_post
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path, alert: "Blog post not found"
   end
@@ -24,13 +26,13 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    @blog_post = BlogPost.find(params[:id])
+    @set_blog_post
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path, alert: "Blog post not found"
   end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
+    @set_blog_post
 
     if @blog_post.update(blog_post_params)
       redirect_to blog_post_path(@blog_post)
@@ -39,10 +41,23 @@ class BlogPostsController < ApplicationController
     end
   end
 
+  def destroy
+    @set_blog_post
+    @blog_post.destroy
+
+    redirect_to root_path
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, alert: "Blog post not found"
+  end
+
   private
 
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
+  end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
   end
 end
 
